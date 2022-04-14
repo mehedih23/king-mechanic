97bab405493cb28501d0355ee8d5222020d857fb
mehedih23 <nmehedi23@gmail.com>
2022-04-14 13:49:57 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // content: [
+  //   "./src/**/*.{js,jsx,ts,tsx}",
+  // ]
+  content: [
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ]
+}
